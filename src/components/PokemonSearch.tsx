bd07c67d8afbf08872f2f183d01e26f261964ad0
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-06-30 15:18:35 -0400

    update

---
+++ src/components/PokemonSearch.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PokemonSearch extends Component {
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default PokemonSearch;
